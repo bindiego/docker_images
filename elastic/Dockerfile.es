d8930a2fff8586ee7e3517fa835bb6ce692930a0
Bin Wu <bin.wu@elastic.co>
2018-12-21 02:15:58 +0000

    initial checkin for elasticsearch container

---
+++ elastic/Dockerfile.es
@@ -0,0 +1,39 @@
+FROM bwjdk:11
+
+USER root
+
+ENV ES_VER 6.5.4
+
+RUN sysctl -w vm.max_map_count=262144
+RUN swapoff -a
+
+# setup application deployment directories
+RUN mkdir -p /bwtmp
+RUN mkdir -p /opt/elasticsearch
+RUN mkdir -p /data/es/logs
+
+RUN curl \
+    https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VER.tar.gz \
+    --output /bwtmp/elasticsearch.tar.gz
+
+RUN tar xzf /bwtmp/elasticsearch.tar.gz -C /bwtmp && \
+    cp -af /bwtmp/elasticsearch-$ES_VER/* /opt/elasticsearch && \
+    rm -rf /bwtmp/*
+
+RUN mkdir -p /opt/elasticsearch/config/certs
+
+COPY conf/*.p12 /opt/elasticsearch/config/certs/
+COPY conf/es.jvm.options /opt/elasticsearch/config/jvm.options
+COPY bin/es.sh /opt/elasticsearch/bin/
+
+RUN useradd elastic
+RUN usermod -aG sudo elastic
+RUN chown -R elastic /opt
+RUN chown -R elastic /data
+
+USER elastic
+
+VOLUME /data
+
+ENTRYPOINT ["/opt/elasticsearch/bin/es.sh"]
+# CMD ["test"]
---
+++ elastic/bin/es.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Author: Bin Wu <bin.wu@elastic.co>
+
+PWD=`pwd`
+JAVA=`which java`
+IPADDR=$(hostname -I | cut -d ' ' -f 1)
+
+echo -n "Starting elasticsearch ... "
+
+ES_JAVA_OPTS="-Xms1g -Xmx1g -XX:-UseConcMarkSweepGC -XX:-UseCMSInitiatingOccupancyOnly -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=75" \
+    /opt/elasticsearch/bin/elasticsearch \
+    -p /opt/es.pid \
+    -Ecluster.name=bindigo \
+    -Enode.name=tiger \
+    -Epath.data=/data/es \
+    -Epath.logs=/data/es/logs \
+    -Enetwork.host=0.0.0.0
+    #-Expack.security.transport.ssl.enabled=true \
+    #-Expack.security.transport.ssl.verification_mode=certificate \
+    #-Expack.security.transport.ssl.keystore.path=certs/elastic-certificates.p12 \
+    #-Expack.security.transport.ssl.truststore.path=certs/elastic-certificates.p12
+    #-Ebootstrap.memory_lock=true
+    #-d -p /opt/es.pid \
+
+if [ $? -eq 0 ]
+then
+    PID=`ps -elf | egrep "elasticsearch" | egrep -v "es.sh|grep" | awk '{print $4}'`
+    echo "succeed, pid: $PID"
+else
+    echo "failed."
+fi
+
