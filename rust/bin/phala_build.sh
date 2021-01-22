caf794754e687473d6e698fb99e36b11a781f608
Bin Wu <binwu@google.com>
2021-01-22 10:55:29 +0000

    added polkadot runner

--- rust/bin/phala_build.sh
+++ rust/bin/phala_build.sh
@@ -2,7 +2,6 @@
 
 echo "phala_build.sh"
 
-#[ -d phala-blockchain ] || git clone https://github.com/Tenet-X/phala-blockchain.git; \
 [ -d phala-blockchain ] || git clone --depth 1 --recurse-submodules --shallow-submodules -j 8 https://github.com/Tenet-X/phala-blockchain.git; \
     cd phala-blockchain \
     && git checkout $BRANCH \
